96e89f9b73e95fe74c152d288d70b85c45d4a6be
Christopher R Murray <christopher.r.murray@protonmail.com>
2022-09-01 12:10:02 -0400

    add login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/react', function () {
+    return view('react');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
