6adcc5d8f389f77c6db13fc06a20feb79146d2d5
Christopher R Murray <christopher.r.murray@protonmail.com>
2024-02-16 20:27:18 -0500

    fix api route

---
+++ dist/assets/Merchants-28be8e13.js
@@ -0,0 +1,1 @@
+import{a as e,j as s,o as t}from"./index-80ebc5db.js";const a={name:"Merchants"},c={class:"d-flex w-100 flex-wrap"};function n(o,r,p,_,f,d){return t(),s("div",c," Merchants page ")}const i=e(a,[["render",n]]);export{i as default};
